758e748be8f4a4ad094bbc6b326a5f29675eb61f
baizhongyang <baizhongyang@vchangyi.com>
2019-04-22 20:29:02 +0800

    初次提交信息

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+
+// echo date('Y-m-d', strtotime(date('Y-m', time()) . '-01 00:00:00')); var_export('--------------------');
+//  echo strtotime(date('Y-m-d', time()) . '-01 00:00:00');
+
+
+// echo date('Y-m-d', strtotime(date('Y-m', time()) . '-' . date('t', time()) . ' 00:00:00'));
+
+//获取本月开始的时间戳
+$beginThismonth=mktime(0,0,0,date('m')+1,1,date('Y'));
+//获取本月结束的时间戳
+$endThismonth=mktime(23,59,59,date('m')+1,date('t'),date('Y'))*1000;
+$a = date('t');
+// $a = date('t');
+var_export($a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
