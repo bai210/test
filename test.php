3b75b327a3668942c885de816f9b5d2776ab1180
baizhongyang <baizhongyang@vchangyi.com>
2019-04-25 16:09:55 +0800

    201904255修改提交

--- test.php
+++ test.php
@@ -9,14 +9,8 @@
 //获取本月开始的时间戳
 $beginThismonth=mktime(0,0,0,date('m')+1,1,date('Y'));
 //获取本月结束的时间戳
-$endThismonth=mktime(23,59,59,date('m')+1,date('t'),date('Y'))*1000;
-
-// $a = date('t');
+$endThismonth=mktime(23,59,59,date('m')+1,date('t'),date('Y'));
 var_export($beginThismonth);
-
-
-
-
 
 
 
